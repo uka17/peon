74689a9b049cc292d323389e8625c02fa80389d8
Maj. Tom Johnson <ukaoneseven@gmail.com>
2018-06-01 00:08:19 +0300

    Separate models for schedules

--- app/models/app_models.js
+++ app/models/app_models.js
@@ -1,4 +1,11 @@
-//models/job.js
+//models/app_models.js
+/*
+Purpose of this schema validator is to check if input from API side (client side): 
+- contains all needed fields
+- fields have correct data format
+- input do not contains extra fields
+It is not relating to DB schema validation anyhow
+*/
 var Ajv = require('ajv');
 
 module.exports.jobSchema = {
@@ -8,10 +15,6 @@ module.exports.jobSchema = {
     name: {type: 'string'},
     description: {type: 'string'},
     enabled: {type: 'boolean'},
-    createdOn: {type: 'string', format: 'date-time'},
-    createdBy: {type: 'string'},
-    modifiedOn: {type: 'string', format: 'date-time'},
-    modifiedBy: {type: 'string'},
     steps: { "type": "array"}  
   },
   additionalProperties: false
@@ -22,14 +25,10 @@ module.exports.stepSchema = {
   type: "object",
   properties: {
     name: {type: 'string'},
-    connection: {type: 'string'},
+    enabled: {type: 'boolean'},      
+    connection: {type: 'object'},
     database: {type: 'string'},
-    command: {type: 'string'},
-    enabled: {type: 'boolean'},          
-    createdOn: {type: 'string', format: 'date-time'},
-    createdBy: {type: 'string'},
-    modifiedOn: {type: 'string', format: 'date-time'},
-    modifiedBy: {type: 'string'}, 
+    command: {type: 'string'},    
     onSucceed: {oneOf: [
       { enum: ['gotoNextStep', 'quitWithSuccess', 'quitWithFailure'] },
       { type: 'object', properties: { gotoStep: {type: 'integer', minimum: 1}}}
@@ -53,36 +52,73 @@ module.exports.scheduleSchema = {
   properties: {
     name: {type: 'string'},
     enabled: {type: 'boolean'},
-    type: { enum: ['oneTime', 'recurrent'] },
-    oneTime: {type: 'string', format: 'date-time'},
-    recurrent: {
-      type: 'object', 
-      properties: { 
-        occurs: {enum: ['daily', 'weekly', 'monthly']},
-        recursEvery: {type: 'integer', minimum: 1}, //dependent on occurs. E.g. every 2nd week
-        dayOfWeek: {
-          type: 'array',
-          uniqueItems: true,
-          items: { enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] },
-          additionalItems: false
-        },
-        dailyFrequency: {oneOf: [
-          { type: 'object', properties: { occursOnceAt: {type: 'time'}}},
-          { type: 'object', properties: { occursEvery: 
-              { type: 'object', properties: { 
-                  intervalType: { enum: ['minute', 'hour'] },
-                  interval: {type: 'integer', minimum: 0},
-                }
-              },
-            }
-          }
-        ]}
-      }
-    },
-    createdOn: {type: 'string', format: 'date-time'},
-    createdBy: {type: 'string'},
-    modifiedOn: {type: 'string', format: 'date-time'},
-    modifiedBy: {type: 'string'}
+    occurrenceType: {oneOf: [
+      { oneTime: {type: 'string', format: 'date-time'}},
+      { recurent: { type: 'object' }}
+    ]},    
   },
   additionalProperties: false
 };
+
+module.exports.scheduleSchemaDaily = {
+  $id: "Daily",
+  type: 'object', 
+  properties: { 
+    eachNDay: {type: 'integer', minimum: 1},
+    dailyFrequency: {type: 'object'}
+  },
+  additionalProperties: false
+};
+
+module.exports.scheduleSchemaWeekly = {
+  $id: "Weekly",
+  type: 'object', 
+  properties: { 
+    eachNWeek: {type: 'integer', minimum: 1},
+    dayOfWeek: {
+      type: 'array',
+      uniqueItems: true,
+      items: { enum: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'] },
+      additionalItems: false
+    },
+    dailyFrequency: {type: 'object'}
+  },
+  additionalProperties: false
+};
+
+module.exports.scheduleSchemaMonthly = {
+  $id: "Monthly",
+  type: 'object', 
+  properties: { 
+    month: {
+      type: 'array',
+      uniqueItems: true,
+      items: { enum: ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'] },
+      additionalItems: false
+    },
+    //TODO check feb and short months
+    day: {type: 'integer', minimum: 1, maximum: 31},
+    dailyFrequency: {type: 'object'}
+  },
+  additionalProperties: false
+};
+
+module.exports.scheduleSchemaDailyFrequency = {
+  $id: "dailyFrequency",
+  type: 'object', 
+  properties: { 
+    dailyFrequency: {oneOf: [
+      { type: 'object', properties: { occursOnceAt: {type: 'time'}}},
+      { type: 'object', properties: { occursEvery: 
+          { type: 'object', properties: { 
+              //TODO check for 24 and 59
+              intervalType: { enum: ['minute', 'hour'] },
+              interval: {type: 'integer', minimum: 0},
+            }
+          },
+        }
+      }
+    ]}
+  },
+  additionalProperties: false
+}
--- app/routes/dummy_routes.js
+++ app/routes/dummy_routes.js
@@ -3,7 +3,7 @@ var mongo = require('mongodb');
 var utools = require('../tools/utools');
 var schema = require('../models/app_models');
 const config = require('../../config/config');
-//var Ajv = require('ajv');
+var Ajv = require('ajv');
 
 module.exports = function(app, dbclient) {
   app.get('/dummy', (req, res) => {
@@ -13,10 +13,7 @@ module.exports = function(app, dbclient) {
       var validate = ajv.compile(schema.scheduleSchema);
       var a = {
         name: 'name',
-        occurs: {
-          recurrentType: 'oneTime', 
-          onceAt: '2016-05-18T16:00:00Z'
-        } 
+        occurrenceType: {oneTime: '2018-05-31T20:54:23.071Z'}
       };
 
       var valid = validate(a);
@@ -30,67 +27,4 @@ module.exports = function(app, dbclient) {
       res.status(500).send({error: e.message});
     }
   });   
-app.get('/dummy1', (req, res) => {
-  var Ajv = require('ajv');
-  var ajv = new Ajv({allErrors: true});
-  
-  var schema = {
-    "type": "object",
-    "properties": {
-      "occurs": {
-        "type": "object", 
-        "properties": { 
-          "recurrentType": { "enum": ["oneTime", "daily"] }
-        },      
-        "if": { "properties": { "recurrentType": {"const": "oneTime"}}},
-        "then": { 
-          "properties": {"onceAt": {"type": "string", "format": "date-time"}},
-          "required": ["onceAt"]
-        },
-        "else": {
-            "properties": { "recursEvery": {"type": "integer", "minimum": 1}},
-            "required": ["recursEvery"] 
-        }, 
-        "additionalProperties": false   //For some unknown reason it doesn't work. Ajv considers correct properties as additional inside IF
-      }
-    }
-  };
-  
-  var validate = ajv.compile(schema);
-  
-  test({"occurs": {
-            "recurrentType": "oneTime", 
-            "onceAt": "2016-05-18T16:00:00Z"
-          } });
-  
-  function test(data) {
-    var valid = validate(data);
-    if (valid) console.log('Valid!');
-    else console.log('Invalid: ' + ajv.errorsText(validate.errors));
-  }
-});
-
-app.get('/dummy', (req, res) => {
-    //dummy
-    try {       
-      var ajv = new Ajv();
-      var validate = ajv.compile(schema.testSchema);
-      var a = {
-        occurs: {
-          recurrentType: 'oneTime', 
-          onceAt: '2016-05-18T16:00:00Z'
-        } 
-      };
-
-      var valid = validate(a);
-      if (valid) 
-        res.status(200).send({result: "Valid"});
-      else 
-        res.status(500).send({error: ajv.errorsText(validate.errors)});
-      
-    }
-    catch(e) {
-      res.status(500).send({error: e.message});
-    }
-  }); 
-};
+}
