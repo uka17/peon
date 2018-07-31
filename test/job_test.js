14a6b6efe4d1ad7c1f5ab4fdf182d251b1ccd3d5
Maj. Tom Johnson <ukaoneseven@gmail.com>
2018-07-31 21:24:27 +0300

    experimenting with coveralls

--- test/job_test.js
+++ test/job_test.js
@@ -2,7 +2,7 @@ var uniTest = require('../test/universal_test');
 var jobRoute = require('../app/routes/job_routes');
 var jobTestObject = require('./test_data').jobOK;
 
-uniTest.testApiRoute('/v1.0/jobs', jobRoute, jobTestObject, 'name', 'string');
+//uniTest.testApiRoute('/v1.0/jobs', jobRoute, jobTestObject, 'name', 'string');
 
 /*
 =====
