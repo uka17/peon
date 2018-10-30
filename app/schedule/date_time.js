798eb1b6afcfde178b53686d974ec87408193a51
Maj. Tom Johnson <ukaoneseven@gmail.com>
2018-10-30 15:49:09 +0300

    Update date_time.js
    =( IV

--- app/schedule/date_time.js
+++ app/schedule/date_time.js
@@ -46,12 +46,14 @@ function addDate(date, years, months = 0, days = 0, hours = 0, minutes = 0, seco
     result.setSeconds(date.getSeconds() + seconds);
     result.setMilliseconds(date.getMilliseconds());
     //patch for some servers (ubuntu) to correctly process leap years
+    /*
     let yearToCheck = result.getFullYear();
     if(!(((yearToCheck % 4 == 0) && (yearToCheck % 100 != 0)) || (yearToCheck % 400 == 0)) && result.getDate() == 29 && result.getMonth() == 2) {
         //set 03-01 instead 29-02
         result.setMonth(2);
         result.setDate(1);
     }
+    */
     return new Date(result);
 }
 module.exports.addDate = addDate;
