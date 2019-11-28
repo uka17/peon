72655ae145befef259645c93dba2bdfd1453b4cc
Major Tom Johnson <ukaoneseven@gmail.com>
2019-11-28 23:19:57 +0300

    CORS support added

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ const app = require('./app/tools/util').expressInstance();
 const dbclient = require('./app/tools/db');
 const main = require('./app/engines/main');
 const log = require('./log/dispatcher');
+const cors = require('cors');
 
+app.use(cors({
+  origin: 'http://localhost:9000'
+}));
 index(app, dbclient);
+
 app.listen(config.port, () => {
   log.info(`We are live on ${config.port}.`);
 });
