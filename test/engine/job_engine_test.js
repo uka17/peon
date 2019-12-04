ea2a167592d53e645dd7097bde8532d57b107ec9
Major Tom Johnson <ukaoneseven@gmail.com>
2019-12-04 23:42:11 +0300

    Job engine test was enhanced

--- test/engine/job_engine_test.js
+++ test/engine/job_engine_test.js
@@ -1,21 +1,20 @@
 /* eslint-disable no-undef */
-let jobId, job;
+let job;
+let testData = require('../test_data');
+let assert  = require('chai').assert;
 const jobEngine = require('../../app/engines/job');
 const jobTestObject = require('../test_data').jobOK;
-let assert  = require('chai').assert;
 const config = require('../../config/config');
 
 describe('1 job engine', function() {
-  /*
   before(async () => {
     job = await jobEngine.createJob(testData.jobOK, config.testUser);
   }); 
 
-  it('1.1 execute job', (done) => {
-    jobEngine.executeJob(objectId, config.testUser);
+  it('1.1 execute job. Success', (done) => {
+    jobEngine.executeJob(job, config.testUser);
     done();
   });      
-  */
   
   it('1.2 calculateNextRun. Failed to validate schedule', (done) => {
     let job = JSON.parse(JSON.stringify(jobTestObject));
