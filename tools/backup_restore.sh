e20ff5e803fcbcbe4e072111fa7f5c4e1f4b35c4
uka17 <ukaoneseven@gmail.com>
2021-06-16 00:22:30 +0300

    Backuping DB

--- tools/backup_restore.sh
+++ tools/backup_restore.sh
@@ -2,7 +2,11 @@
 
 #backup db
 pg_dump -h 172.17.0.2 -p 5432 -U postgres -O -F p -b -v -f "./peon.sql" peon
-#reset db at heroku
-heroku pg:reset postgresql-rigid-10499 --app myjobkeeper --confirm myjobkeeper
-#restore db
-psql -h ec2-54-217-234-157.eu-west-1.compute.amazonaws.com -p 5432 -U lkabdjtptaesng -d de61oteg9ukstn -f "./peon.sql"
+if [ $? -eq 0 ]; then
+    #reset db at heroku
+    heroku pg:reset postgresql-rigid-10499 --app myjobkeeper --confirm myjobkeeper
+    #restore db
+    psql -h ec2-54-217-234-157.eu-west-1.compute.amazonaws.com -p 5432 -U lkabdjtptaesng -d de61oteg9ukstn -f "./peon.sql"
+else
+    exit 1
+fi
