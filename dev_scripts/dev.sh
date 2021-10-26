65fc42e620efe4bc1a73a73ea78b4efa0704e86e
Nikolai Kozhekhov <ukaoneseven@gmail.com>
2021-10-27 00:06:14 +0300

    Dev script updated

--- dev_scripts/dev.sh
+++ dev_scripts/dev.sh
@@ -3,12 +3,15 @@
 #Launch all needed things for development
 tmux new -s dev -d
 tmux split-window -v -t dev:0.0
+tmux split-window -v -t dev:0.1
 tmux send-keys -t dev:0.0 'docker start postgres' Enter
 tmux send-keys -t dev:0.0 'docker start mongo' Enter
 tmux send-keys -t dev:0.0 'cd ~/_code/peon/' Enter
-tmux send-keys -t dev:0.0 'npm run dev' Enter
-tmux send-keys -t dev:0.1 'cd ~/_code/peon-web/' Enter
-tmux send-keys -t dev:0.1 'npm run start' Enter
+tmux send-keys -t dev:0.0 'npm run dev-srv' Enter
+tmux send-keys -t dev:0.1 'cd ~/_code/peon/' Enter
+tmux send-keys -t dev:0.1 'npm run dev-api' Enter
+tmux send-keys -t dev:0.2 'cd ~/_code/peon-web/' Enter
+tmux send-keys -t dev:0.2 'npm run start' Enter
 xdg-open http://localhost:9000
 xdg-open https://trello.com/b/sWc07nUn/peon-web
 tmux attach
